c1419f89ef729e517960e72b4303ef9154d4bcd5
Keda87 <adiyatmubarak@gmail.com>
2019-03-22 11:54:14 +0700

    Update book

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import uuid
+
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
@@ -11,21 +13,33 @@ CORS(app)
 
 BOOKS = [
     {
+        'id': uuid.uuid4().hex,
         'title': 'On the Road',
         'author': 'Jack Kerouac',
         'read': True
     },
     {
+        'id': uuid.uuid4().hex,
         'title': 'Harry Potter and the Philosopher\'s Stone',
         'author': 'J. K. Rowling',
         'read': False
     },
     {
+        'id': uuid.uuid4().hex,
         'title': 'Green Eggs and Ham',
         'author': 'Dr. Seuss',
         'read': True
     }
 ]
+
+
+def remove_book(book_id):
+    for book in BOOKS:
+        if book['id'] == book_id:
+            BOOKS.remove(book)
+            return True
+    return False
+
 
 @app.route('/books', methods=['get', 'post'])
 def all_books():
@@ -40,6 +54,21 @@ def all_books():
         response_object['books'] = 'Book added!'
     else:
         response_object['books'] = BOOKS
+    return jsonify(response_object)
+
+@app.route('/books/<book_id>', methods=['put'])
+def single_book(book_id):
+    response_object = {'status': 'success'}
+    if request.method == 'PUT':
+        post_data = request.get_json()
+        remove_book(book_id)
+        BOOKS.append({
+            'id': uuid.uuid4().hex,
+            'title': post_data.get('title'),
+            'author': post_data.get('author'),
+            'read': post_data.get('read')
+        })
+        response_object['message'] = 'Book updated!'
     return jsonify(response_object)
 
 
--- client/src/components/Books.vue
+++ client/src/components/Books.vue
@@ -28,7 +28,10 @@
                 <span v-else>No</span>
               </td>
               <td>
-                <button type="button" class="btn btn-warning btn-sm">Update</button>
+                <button type="button"
+                        class="btn btn-warning btn-sm"
+                        v-b-modal.book-update-modal
+                        @click="editBook(book)">Update</button>
                 <button type="button" class="btn btn-danger btn-sm">Delete</button>
               </td>
             </tr>
@@ -38,7 +41,7 @@
     </div>
     <!-- End of book content -->
 
-    <!-- Book modal form -->
+    <!-- Add book modal form -->
     <b-modal ref="addBookModal"
              id="book-modal"
              title="Add a new book"
@@ -77,7 +80,48 @@
 
       </b-form>
     </b-modal>
-    <!-- End of book modal form -->
+    <!-- End of add book modal form -->
+
+    <!-- Update book modal form -->
+    <b-modal ref="editBookModal"
+             id="book-update-modal"
+             title="Update"
+             hide-footer>
+      <b-form @submit="onSubmitUpdate" @reset="onResetUpdate" class="w-100">
+
+        <b-form-group id="form-title-edit-group"
+                label="Title:"
+                label-for="form-title-edit-input">
+          <b-form-input id="form-title-edit-input"
+                        type="text"
+                        v-model="editForm.title"
+                        required
+                        placeholder="Enter title">
+          </b-form-input>
+        </b-form-group>
+
+        <b-form-group id="form-author-edit-group"
+                      label="Author:"
+                      label-for="form-author-edit-input">
+          <b-form-input id="form-author-edit-input"
+                        type="text"
+                        v-model="editForm.author"
+                        required
+                        placeholder="Enter author">
+          </b-form-input>
+        </b-form-group>
+
+        <b-form-group id="form-read-edit-group">
+          <b-form-checkbox-group v-model="editForm.read" id="form-checks">
+            <b-form-checkbox value="true">Read?</b-form-checkbox>
+          </b-form-checkbox-group>
+        </b-form-group>
+
+        <b-button type="submit" variant="primary">Update</b-button>
+        <b-button type="reset" variant="danger">Cancel</b-button>
+      </b-form>
+    </b-modal>
+    <!-- End of update book modal form -->
   </div>
 </template>
 
@@ -90,6 +134,12 @@ export default {
     return {
       books: [],
       addBookForm: {
+        title: '',
+        author: '',
+        read: [],
+      },
+      editForm: {
+        id: '',
         title: '',
         author: '',
         read: [],
@@ -131,6 +181,10 @@ export default {
       this.addBookForm.title = '';
       this.addBookForm.author = '';
       this.addBookForm.read = [];
+      this.editForm.id = '';
+      this.editForm.title = '';
+      this.editForm.author = '';
+      this.editForm.read = [];
     },
     onSubmit(evt) {
       evt.preventDefault();
@@ -149,6 +203,41 @@ export default {
       evt.preventDefault();
       this.$refs.addBookModal.hide();
       this.initForm();
+    },
+    editBook(book) {
+      this.editForm = book;
+    },
+    onSubmitUpdate(evt) {
+      evt.preventDefault();
+      this.$refs.editBookModal.hide();
+      let read = false;
+      if (this.editForm.read[0]) read = true;
+      const payload = {
+        title: this.editForm.title,
+        author: this.editForm.author,
+        read,
+      };
+      this.updateBook(payload, this.editForm.id);
+    },
+    onResetUpdate(evt) {
+      evt.preventDefault();
+      this.$refs.editBookModal.hide();
+      this.initForm();
+      this.getBooks();
+    },
+    updateBook(payload, bookID) {
+      const path = `http://127.0.0.1:5000/books/${bookID}`;
+      axios.put(path, payload)
+        .then(() => {
+          this.getBooks();
+          this.message = 'Book updated!';
+          this.showMessage = true;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+          this.getBooks();
+        });
     },
   },
   created() {
